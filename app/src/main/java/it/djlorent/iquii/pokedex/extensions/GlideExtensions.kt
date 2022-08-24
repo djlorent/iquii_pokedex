9db478f2a535e5706f6ce5820adb927411e2c0d4
Lorenzo Quinto <lorenzo.quinto@kalpa.it>
2022-08-24 19:56:35 +0200

    installing Glide

---
+++ app/src/main/java/it/djlorent/iquii/pokedex/extensions/GlideExtensions.kt
@@ -0,0 +1,24 @@
+package it.djlorent.iquii.pokedex.extensions
+
+import android.graphics.drawable.Drawable
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
+inline fun RequestBuilder<Drawable>.withSuccessListener(crossinline action: () -> Unit) =
+    addListener(object : RequestListener<Drawable?> {
+        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable?>?, isFirstResource: Boolean) = false
+
+        override fun onResourceReady(
+            resource: Drawable?,
+            model: Any?,
+            target: Target<Drawable?>?,
+            dataSource: DataSource?,
+            isFirstResource: Boolean
+        ): Boolean {
+            action()
+            return false
+        }
+    })
