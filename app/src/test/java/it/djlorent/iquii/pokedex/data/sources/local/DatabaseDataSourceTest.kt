85260db7a282d8116befa728c58a2e01eb9dff22
Lorenzo Quinto <lorenzo.quinto@kalpa.it>
2022-08-24 14:31:06 +0200

    updated Local Data Source tests

--- app/src/test/java/it/djlorent/iquii/pokedex/data/sources/local/DatabaseDataSourceTest.kt
+++ app/src/test/java/it/djlorent/iquii/pokedex/data/sources/local/DatabaseDataSourceTest.kt
@@ -5,6 +5,7 @@ import it.djlorent.iquii.pokedex.data.sources.local.database.dao.FavoritesDao
 import it.djlorent.iquii.pokedex.data.sources.local.database.dao.PokedexDao
 import it.djlorent.iquii.pokedex.data.sources.local.database.entities.Favorite
 import it.djlorent.iquii.pokedex.models.Pokemon
+import it.djlorent.iquii.pokedex.utils.MockUtils
 import it.djlorent.iquii.pokedex.data.sources.local.database.entities.Pokemon as LocalPokemon
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
@@ -32,192 +33,6 @@ class DatabaseDataSourceTest {
 
     lateinit var localDataSource: DatabaseDataSource
 
-    private val pokedexMock = listOf(
-        Pokemon(
-            name = "bulbasaur",
-            id = 1,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-        ),
-        Pokemon(
-            name = "ivysaur",
-            id = 2,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"
-        ),
-        Pokemon(
-            name = "venusaur",
-            id = 3,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"
-        ),
-        Pokemon(
-            name = "charmander",
-            id = 4,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
-        ),
-        Pokemon(
-            name = "charmeleon",
-            id = 5,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
-        ),
-        Pokemon(
-            name = "charizard",
-            id = 6,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
-        ),
-        Pokemon(
-            name = "squirtle",
-            id = 7,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
-        ),
-        Pokemon(
-            name = "wartortle",
-            id = 8,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png"
-        ),
-        Pokemon(
-            name = "blastoise",
-            id = 9,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png"
-        ),
-        Pokemon(
-            name = "caterpie",
-            id = 10,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png"
-        ),
-        Pokemon(
-            name = "metapod",
-            id = 11,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png"
-        ),
-        Pokemon(
-            name = "butterfree",
-            id = 12,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png"
-        ),
-        Pokemon(
-            name = "weedle",
-            id = 13,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png"
-        ),
-        Pokemon(
-            name = "kakuna",
-            id = 14,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/14.png"
-        ),
-        Pokemon(
-            name = "beedrill",
-            id = 15,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png"
-        ),
-        Pokemon(
-            name = "pidgey",
-            id = 16,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png"
-        ),
-        Pokemon(
-            name = "pidgeotto",
-            id = 17,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/17.png"
-        ),
-        Pokemon(
-            name = "pidgeot",
-            id = 18,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/18.png"
-        ),
-        Pokemon(
-            name = "rattata",
-            id = 19,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png"
-        ),
-        Pokemon(
-            name = "raticate",
-            id = 20,
-            image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/20.png"
-        ),
-    )
-
-    private val pokedexDbMock = listOf(
-        LocalPokemon(
-            name = "bulbasaur",
-            id = 1
-        ),
-        LocalPokemon(
-            name = "ivysaur",
-            id = 2
-        ),
-        LocalPokemon(
-            name = "venusaur",
-            id = 3
-        ),
-        LocalPokemon(
-            name = "charmander",
-            id = 4
-        ),
-        LocalPokemon(
-            name = "charmeleon",
-            id = 5
-        ),
-        LocalPokemon(
-            name = "charizard",
-            id = 6
-        ),
-        LocalPokemon(
-            name = "squirtle",
-            id = 7
-        ),
-        LocalPokemon(
-            name = "wartortle",
-            id = 8
-        ),
-        LocalPokemon(
-            name = "blastoise",
-            id = 9
-        ),
-        LocalPokemon(
-            name = "caterpie",
-            id = 10
-        ),
-        LocalPokemon(
-            name = "metapod",
-            id = 11
-        ),
-        LocalPokemon(
-            name = "butterfree",
-            id = 12
-        ),
-        LocalPokemon(
-            name = "weedle",
-            id = 13
-        ),
-        LocalPokemon(
-            name = "kakuna",
-            id = 14
-        ),
-        LocalPokemon(
-            name = "beedrill",
-            id = 15
-        ),
-        LocalPokemon(
-            name = "pidgey",
-            id = 16
-        ),
-        LocalPokemon(
-            name = "pidgeotto",
-            id = 17
-        ),
-        LocalPokemon(
-            name = "pidgeot",
-            id = 18
-        ),
-        LocalPokemon(
-            name = "rattata",
-            id = 19
-        ),
-        LocalPokemon(
-            name = "raticate",
-            id = 20
-        ),
-    )
-
     @Before
     fun setup() = runBlocking {
         MockitoAnnotations.openMocks(this)
@@ -230,15 +45,16 @@ class DatabaseDataSourceTest {
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun gettingPokedex() = runTest {
+        val  dbMock = MockUtils.pokedexDbMock()
         Mockito
             .`when`(pokedexDao.getAll())
-            .thenReturn(pokedexDbMock)
+            .thenReturn(dbMock)
 
         val result = localDataSource.getPokedex()
         result.forEach(::println)
 
         assert(result.isNotEmpty())
-        assert(result.size == pokedexDbMock.size)
+        assert(result.size == dbMock.size)
         assert(result.first().id == 1)
         assert(result.last().id == 20)
     }
@@ -251,7 +67,7 @@ class DatabaseDataSourceTest {
 
         Mockito
             .`when`(pokedexDao.getAll(page,pageSize))
-            .thenReturn(pokedexDbMock.subList(0, 10))
+            .thenReturn(MockUtils.pokedexDbMock().subList(0, 10))
 
         val result = localDataSource.getPokedex(page, pageSize)
         result.forEach(::println)
@@ -270,7 +86,7 @@ class DatabaseDataSourceTest {
 
         Mockito
             .`when`(pokedexDao.getAll(page,pageSize))
-            .thenReturn(pokedexDbMock.subList(10, 20))
+            .thenReturn(MockUtils.pokedexDbMock().subList(10, 20))
 
         val result = localDataSource.getPokedex(page, pageSize)
         result.forEach(::println)
@@ -285,10 +101,10 @@ class DatabaseDataSourceTest {
     @Test
     fun addingToPokedex() = runTest {
         Mockito
-            .`when`(pokedexDao.insert(pokedexDbMock.subList(0, 10)))
+            .`when`(pokedexDao.insert(MockUtils.pokedexDbMock().subList(0, 10)))
             .thenReturn(listOf(0,1,2,3,4,5,6,7,8,9))
 
-        val result = localDataSource.addToPokedex(pokedexMock.subList(0, 10))
+        val result = localDataSource.addToPokedex(MockUtils.pokedexMock().subList(0, 10))
 
         assert(result)
     }
@@ -298,7 +114,7 @@ class DatabaseDataSourceTest {
     fun gettingFavorites() = runTest {
         Mockito
             .`when`(favoritesDao.getAll())
-            .thenReturn(pokedexDbMock.subList(5, 10))
+            .thenReturn(MockUtils.pokedexDbMock().subList(5, 10))
 
         val result = localDataSource.getFavorites()
         result.forEach(::println)
@@ -317,7 +133,7 @@ class DatabaseDataSourceTest {
 
         Mockito
             .`when`(favoritesDao.getAll(page,pageSize))
-            .thenReturn(pokedexDbMock.subList(5, 10))
+            .thenReturn(MockUtils.pokedexDbMock().subList(5, 10))
 
         val result = localDataSource.getFavorites(page, pageSize)
         result.forEach(::println)
@@ -334,13 +150,13 @@ class DatabaseDataSourceTest {
         val pokemonIdMock = 3
         Mockito
             .`when`(pokedexDao.getById(pokemonIdMock))
-            .thenReturn(pokedexDbMock.find { it.id == pokemonIdMock })
+            .thenReturn(MockUtils.pokedexDbMock().find { it.id == pokemonIdMock })
 
         val result = localDataSource.getPokemonDetails(pokemonIdMock)
 
         assert(result != null)
         assert(result!!.id == pokemonIdMock)
-        assert(result.name == pokedexMock.find { it.id == pokemonIdMock }!!.name)
+        assert(result.name == MockUtils.pokedexMock().find { it.id == pokemonIdMock }!!.name)
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
